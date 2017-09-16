c75063496a7714dffadafc940d640b3ba7f4848e
Sam <sam@developersam.com>
2017-09-15 21:36:27 -0400

    Integrate GoogleAnalyzer with ProcessorFactory

--- src/com/developersam/web/chunkreader/processor/ProcessorFactory.java
+++ src/com/developersam/web/chunkreader/processor/ProcessorFactory.java
@@ -1,15 +1,26 @@
 package com.developersam.web.chunkreader.processor;
 
+import com.developersam.web.chunkreader.google.GoogleAnalyzer;
+
+import java.io.IOException;
+
+/**
+ * A factory method to generate different kinds of processors.
+ * It is used to ensure quick switches between different approaches to a same
+ * sub-problem.
+ */
 public class ProcessorFactory {
 
-    private String text;
+    private GoogleAnalyzer googleAnalyzer;
 
     /**
      * Construct the factory by a piece of text.
      * @param text a piece of text to be analyzed.
+     * @throws IOException thrown when there is a problem with
+     * Google Language API.
      */
-    public ProcessorFactory(String text) {
-        this.text = text;
+    public ProcessorFactory(String text) throws IOException {
+        googleAnalyzer = new GoogleAnalyzer(text);
     }
 
     /**
@@ -17,7 +28,9 @@ public class ProcessorFactory {
      * @return a processor with all necessary information initialized.
      */
     public Processor createTypePredictor() {
-        return null;
+        TypePredictor p = null; // TODO when constructor is available.
+        p.read(googleAnalyzer.getSentiment());
+        return p;
     }
 
     /**
@@ -25,7 +38,9 @@ public class ProcessorFactory {
      * @return a processor with all necessary information initialized.
      */
     public Processor createKnowledgeGraphBuilder() {
-        return null;
+        KnowledgeGraphBuilder p = null; // TODO when constructor is available.
+        p.read(googleAnalyzer.getEntities());
+        return p;
     }
 
     /**
@@ -33,6 +48,10 @@ public class ProcessorFactory {
      * @return a processor with all necessary information initialized.
      */
     public Processor createSummaryGenerator() {
+        SummaryGenerator p = null; // TODO when constructor is available.
+        p.read(googleAnalyzer.getEntities(),
+                googleAnalyzer.getSentences(),
+                googleAnalyzer.getTokens());
         return null;
     }
 
