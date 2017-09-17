7deea713eebc55d2799feaee8d91134b8c29ab2a
Sam <sam@developersam.com>
2017-09-17 05:57:41 -0400

    Update UI

--- src/com/developersam/web/devsuit/tags/components/head/HeadTag.java
+++ src/com/developersam/web/devsuit/tags/components/head/HeadTag.java
@@ -32,6 +32,6 @@ public class HeadTag extends TemplateTag {
         printContent("<link rel=\"stylesheet\" href=\"/framework/css/base.css\" />");
         printContent("<script defer src=\"https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.min.js\"></script>");
         printContent("<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js\"></script>");
-        printContent("<link rel=\"shortcut icon\" href=\"/favicon.ico?v=1\" />");
+        printContent("<link rel=\"shortcut icon\" href=\"/favicon.ico?v=2\" />");
     }
 }
